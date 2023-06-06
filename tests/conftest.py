ab1c6df790b0068cf5e7598a5dd601275bc15feb
Duc-Minh Phan <alephvn@gmail.com>
2023-06-06 10:35:35 +0700

    test: Fix `create_database` fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,19 @@
 import os
 import sys
-from pathlib import Path
 
 import pytest
-from alembic.command import upgrade
-from alembic.config import Config
 from sqlalchemy.orm import scoped_session
 
 from main import app as _app
 from main import db
 from main.libs.log import ServiceLogger
+from main.models.base import BaseModel
 
 logger = ServiceLogger(__name__)
 
 if os.getenv("ENVIRONMENT") != "test":
     logger.error(message='Tests should be run with "ENVIRONMENT=test"')
     sys.exit(1)
-
-ALEMBIC_CONFIG = (Path(__file__).parents[1] / "alembic.ini").resolve().as_posix()
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -32,8 +28,10 @@ def app():
 
 @pytest.fixture(scope="session", autouse=True)
 def create_database(app):
-    alembic_config = Config(ALEMBIC_CONFIG)
-    upgrade(alembic_config, "heads")
+    engine = db.session.bind.engine
+
+    BaseModel.metadata.drop_all(engine)
+    BaseModel.metadata.create_all(engine)
 
 
 @pytest.fixture(scope="function", autouse=True)
