f98085801976fe5c90c366b968ee7d1aa768770b
Duc-Minh Phan <alephvn@gmail.com>
2023-09-21 14:28:52 +0700

    feat: Use SQLAlchemy v2 API

--- main/models/base.py
+++ main/models/base.py
@@ -1,28 +1,31 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime, MetaData
-from sqlalchemy.orm import DeclarativeBase, mapped_column
+from sqlalchemy import JSON, MetaData
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class BaseModel(DeclarativeBase):
-    _naming_convention = {
-        "ix": "ix_%(column_0_label)s",
-        "uq": "uq_%(table_name)s_%(column_0_name)s",
-        "ck": "ck_%(table_name)s_%(column_0_name)s",
-        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-        "pk": "pk_%(table_name)s",
+    metadata = MetaData(
+        naming_convention={
+            "pk": "pk_%(table_name)s",
+            "fk": "fk_%(table_name)s_%(column_0_N_name)s_%(referred_table_name)s",
+            "ix": "ix_%(column_0_N_name)s",
+            "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+            "ck": "ck_%(table_name)s_%(column_0_N_name)s",
+        },
+    )
+
+    type_annotation_map = {
+        dict: JSON,
     }
-    metadata = MetaData(naming_convention=_naming_convention)
 
 
 class TimestampMixin:
-    created_at = mapped_column(
-        DateTime,
+    created_at: Mapped[datetime] = mapped_column(
         default=datetime.utcnow,
         nullable=False,
     )
-    updated_at = mapped_column(
-        DateTime,
+    updated_at: Mapped[datetime] = mapped_column(
         default=datetime.utcnow,
         onupdate=datetime.utcnow,
         nullable=False,
--- main/models/log.py
+++ main/models/log.py
@@ -2,7 +2,6 @@
 This file is an example for the sake of completeness, meant to be removed.
 """
 
-from sqlalchemy import JSON, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import BaseModel, TimestampMixin
@@ -11,5 +10,5 @@ from .base import BaseModel, TimestampMixin
 class LogModel(BaseModel, TimestampMixin):
     __tablename__ = "log"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    data: Mapped[dict | None] = mapped_column(JSON, nullable=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    data: Mapped[dict | None] = mapped_column(nullable=True)
